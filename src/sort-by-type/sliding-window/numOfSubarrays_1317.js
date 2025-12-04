173813711a1fac5e105fe2c000f821608e2ac9de
abbylolo <abbylolo@163.com>
2025-12-04 14:43:26 +0800

    【类型】定长滑动窗口 【题目】1343. 大小为 K 且平均值大于等于阈值的子数组数目 【难度】中等-1317

---
+++ src/sort-by-type/sliding-window/numOfSubarrays_1317.js
@@ -0,0 +1,29 @@
+/**
+ * @description 1343. 大小为 K 且平均值大于等于阈值的子数组数目
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} threshold
+ * @return {number}
+ * 
+ * 给你一个整数数组 arr 和两个整数 k 和 threshold 。
+ * 请你返回长度为 k 且平均值大于等于 threshold 的子数组数目。
+ * 
+ * 示例 1：
+ * 输入：arr = [2,2,2,2,5,5,5,8], k = 3, threshold = 4
+ * 输出：3
+ * 解释：子数组 [2,5,5],[5,5,5],[5,5,8] 的平均值分别为 4，5，6 且大于 4 。其他长度为 3 的子数组平均值都小于 4 。
+ */
+
+var numOfSubarrays = function (arr, k, threshold) { 
+    let sum = 0, num = 0
+    for(let right = 0; right < arr.length; right++) {
+        sum += arr[right]
+        let left = right - k + 1
+        if(left < 0) continue
+        if(sum >= threshold * k) {
+            num++
+        }
+        sum -= arr[left]
+    }
+    return num
+}
